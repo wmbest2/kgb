2ac3a8db9e48f0783b966f8f2b5ce300001978fd
Bill Best <bill.best@yello.co>
2019-05-02 14:13:18 -0500

    Adds IDE regions for code

--- src/main/kotlin/kgb/cpu/LR35902.kt
+++ src/main/kotlin/kgb/cpu/LR35902.kt
@@ -9,22 +9,27 @@ class LR35902(
         registers: IRegisters = Registers()
 ): IRegisters by registers {
 
+    // region Status Bits
     var zeroBit by flag(7)
     var subtractBit by flag(6)
     var halfCarryBit by flag(5)
     var carryBit by flag(4)
+    // endregion
 
+    // region Registers
     var stackPointer: UShort = 0xFFFEu // Defaults to last ram point
     var programCounter: UShort = 0x0100u
+    // endregion
 
-    private var cpuHalted = false
+    // region operation flags
+    private var HALT = false
     private var interruptsEnabled = true
-
+    // endregion
 
     fun step() {
-        if (cpuHalted) {
+        if (HALT) {
             // Skip only once if interrupts are disabled [ See gbcpuman.pdf, Page 19 ]
-            if (!interruptsEnabled) cpuHalted = false
+            if (!interruptsEnabled) HALT = false
             return
         }
 
@@ -32,13 +37,13 @@ class LR35902(
         when(val opcode = command.toUInt()) {
             // Commands
             0x00u -> {}  // NoOp
-            0x76u -> cpuHalted = true // HALT
-            0xF3u -> di()
-            0xFBu -> ei()
+            0x76u -> HALT = true
+            0xF3u -> DI()
+            0xFBu -> EI()
 
             // Flag Operations
-            0x37u -> scf()
-            0x3Fu -> ccf()
+            0x37u -> SCF()
+            0x3Fu -> CCF()
 
             // Jump
             0xC3u -> `JP (a16)`()
@@ -72,29 +77,37 @@ class LR35902(
         }
     }
 
-    private fun di() {
+    //region Opcodes
+
+    //region Commands
+    private fun DI() {
         interruptsEnabled = false
     }
 
-    private fun ei() {
+    private fun EI() {
         interruptsEnabled = true
     }
 
-    private fun scf() {
+    private fun SCF() {
         carryBit = true
         halfCarryBit = false
         subtractBit = false
     }
 
-    private fun ccf() {
+    private fun CCF() {
         halfCarryBit = false
         carryBit = !carryBit
         subtractBit = false
     }
+    //endregion
 
+    // region Jump Ops
     private fun `JP (a16)`() {
         programCounter = asWord(memory[programCounter++], memory[programCounter++])
     }
+    // endregion
+
+    // region Load Ops
 
     private fun `LD n, d8`(opcode: UInt) {
         val value = memory[programCounter++]
@@ -109,6 +122,59 @@ class LR35902(
         }
     }
 
+    private fun `LD A, (a16)`() {
+        val low = memory[programCounter++]
+        val high = memory[programCounter++]
+        A = memory[asWord(low, high)]
+    }
+
+    private fun `LD r1, r2`(opcode: UInt) {
+        val writeOffset = (opcode - 0x40u) / 8u
+        val readOffset = opcode % 8u
+        writeRegisterBy(writeOffset, readRegisterBy(readOffset))
+    }
+    // endregion
+
+    // region Math Ops
+
+    private fun `OR r`(opcode: UInt) {
+        val offset = opcode % 8u
+        A = readRegisterBy(offset) or A
+
+        zeroBit = A == 0x0u.toUByte()
+        carryBit = false
+        halfCarryBit = false
+        subtractBit = false
+    }
+
+    // endregion
+
+    // region Bit Ops
+
+    private fun `BIT b, n`(opcode: UInt) {
+        val offset = opcode % 8u
+        val bit = (opcode - 0x40u) / 8u
+
+        halfCarryBit = true
+        carryBit = false
+        zeroBit = !readRegisterBy(offset).bit(bit.toInt())
+    }
+
+    private fun `RES b, n`(opcode: UInt) {
+        val offset = opcode % 8u
+        val bit = (opcode - 0x80u) / 8u
+        setBitForRegisterTo(offset, bit.toInt(), false)
+    }
+
+    private fun `SET b, n`(opcode: UInt) {
+        val offset = opcode % 8u
+        val bit = (opcode - 0xC0u) / 8u
+        setBitForRegisterTo(offset, bit.toInt(), true)
+    }
+    //endregion
+    //endregion
+
+    // region Helpers
     /**
      * Many commands use the same offset scheme to determine
      * which register to write.
@@ -148,50 +214,10 @@ class LR35902(
         }
     }
 
-    private fun `LD A, (a16)`() {
-        val low = memory[programCounter++]
-        val high = memory[programCounter++]
-        A = memory[asWord(low, high)]
-    }
-
-    private fun `LD r1, r2`(opcode: UInt) {
-        val writeOffset = (opcode - 0x40u) / 8u
-        val readOffset = opcode % 8u
-        writeRegisterBy(writeOffset, readRegisterBy(readOffset))
-    }
-
-    private fun `OR r`(opcode: UInt) {
-        val offset = opcode % 8u
-        A = readRegisterBy(offset) or A
-
-        zeroBit = A == 0x0u.toUByte()
-        carryBit = false
-        halfCarryBit = false
-        subtractBit = false
-    }
-
     private fun setBitForRegisterTo(offset: UInt, bit: Int, value: Boolean) {
         writeRegisterBy(offset, readRegisterBy(offset).withBit(bit, value))
     }
 
-    private fun `BIT b, n`(opcode: UInt) {
-        val offset = opcode % 8u
-        val bit = (opcode - 0x40u) / 8u
+    //endregion
 
-        halfCarryBit = true
-        carryBit = false
-        zeroBit = !readRegisterBy(offset).bit(bit.toInt())
-    }
-
-    private fun `RES b, n`(opcode: UInt) {
-        val offset = opcode % 8u
-        val bit = (opcode - 0x80u) / 8u
-        setBitForRegisterTo(offset, bit.toInt(), false)
-    }
-
-    private fun `SET b, n`(opcode: UInt) {
-        val offset = opcode % 8u
-        val bit = (opcode - 0xC0u) / 8u
-        setBitForRegisterTo(offset, bit.toInt(), true)
-    }
 }
